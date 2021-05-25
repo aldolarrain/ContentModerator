a9a2f27912d2f2a5c0c4293a44244209e1d769b5
aldolarrain <a2016114029@estudiantes.upsa.edu.bo>
2021-05-24 21:58:17 -0400

    Agregar archivos de proyecto.

---
+++ TextModeration/Helpers/Globals.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace TextModeration
+{
+    class Globals
+    {
+        public enum CallType { POST, GET };
+
+        public const int CALLSTATUSOK = 200;
+        public const int CALLVOLUMEEXCEEDED = 403;
+        public const int CALLRATEEXCEEDED = 429;
+        public const int CALLWAITTIME = 1000;
+
+        public const string TOP_DIR = @"C:\Users\Aldo\Desktop\ContentModerator-API-Samples-master\ContentModerator-API-Samples-master\TextModeration\Sample files\";
+        public const string INPUT_FILE = TOP_DIR + "input.txt";
+        public const string OUTPUT_JSONFILE = TOP_DIR + "output7";
+
+        //Content Moderator Key 
+        public const string CONTENTMODERATOR_APIKEY = Secrets.CONTENTMODERATOR_APIKEY;
+
+        // All Uris   
+        public const string APIURI = "https://moderadoraldo.cognitiveservices.azure.com/contentmoderator/moderate/v1.0/ProcessText/Screen";
+
+
+    }
+}
---
+++ TextModeration/Helpers/Secrets.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextModeration
+{
+    class Secrets
+    {
+        //Content Moderator Key 
+        public const string CONTENTMODERATOR_APIKEY = "a57ad8fa46974d0bad65416f03483d6d";
+    }
+}
